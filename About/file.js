7d4abba4c3ff32894bfbb7bcd57da594c8336044
KylieAimee <kylie.farrell@edu.sait.ca>
2024-11-16 23:00:15 -0700

    add js greeting

--- About/file.js
+++ About/file.js
@@ -1,3 +1,14 @@
 const fyear = document.getElementById("footeryear");
 const d = new Date();
 fyear.textContent = "(c) Kylie Farrell " + d.getFullYear();
+
+//need to fix
+const greeting = document.getElementById("Greetingheader") => {
+    if (d.getHours() <= 11) {
+        console.log('Good Morning!');
+    } else if (d.getHours() < 18 ) {
+        console.log('Good Afternoon!');
+    } else if (d.getHours() < 22 ) {
+        console.log('Good Afternoon!')
+    }
+}
