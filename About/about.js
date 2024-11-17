5b09a8464070320b0cf19dbcb9fff6710dfddf68
KylieAimee <kylie.farrell@edu.sait.ca>
2024-11-17 00:14:24 -0700

    fix greeting, add comments

--- About/about.js
+++ About/about.js
@@ -1,13 +1,18 @@
+//footer date
 const fyear = document.getElementById("footeryear");
 const d = new Date();
 fyear.textContent = "(c) Kylie Farrell " + d.getFullYear();
 
-//need to fix
-const greeting = document.getElementById("Greetingheader")
-    if (d.getHours() <= 11) {
-        console.log('Good Morning!');
-    } else if (d.getHours() < 18 ) {
-        console.log('Good Afternoon!');
-    } else if (d.getHours() < 22 ) {
-        console.log('Good Afternoon!')
-    }
+//Greeting
+document.getElementById("greeting");
+const hour = new Date().getHours();
+const welcomeTypes = 
+["Good morning! I'm Kylie", "Good afternoon! I'm Kylie", "Good evening! I'm Kylie"];
+
+let welcomeText = "";
+
+if (hour < 12) welcomeText = welcomeTypes[0];
+else if (hour < 18) welcomeText = welcomeTypes[1];
+else welcomeText = welcomeTypes[2];
+
+greeting.innerHTML = welcomeText;
--- Contact/file.js
+++ Contact/file.js
@@ -1,3 +1,4 @@
+//footer date
 const fyear = document.getElementById("footeryear");
 const d = new Date();
 fyear.textContent = "(c) Kylie Farrell " + d.getFullYear();
