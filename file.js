666419ddb70b84bde815c953a732a27fa95f5d01
KylieAimee <kylie.farrell@edu.sait.ca>
2024-11-17 17:13:07 -0700

    fix console date

--- file.js
+++ file.js
@@ -2,7 +2,5 @@
 const fyear = document.getElementById("footeryear");
 const d = new Date();
 fyear.textContent = "(c) Kylie Farrell " + d.getFullYear();
-
-//console date and time
-const currentDate = new Date();
-console.log(currentDate);
+//console date
+console.log(d);
